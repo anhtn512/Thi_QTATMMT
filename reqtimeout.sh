2ced6490a7389b6d3c1b05c715c61d7c8cb41c45
anhtn512 <anhtn512@gmail.com>
2018-12-22 08:24:20 +0700

    update code with rule suricata

--- reqtimeout.sh
+++ reqtimeout.sh
@@ -2,3 +2,7 @@ sed -i "s|LoadModule version_module modules/mod_version.so|LoadModule version_mo
 sed -i "s|<IfModule mod_userdir.c>|<IfModule reqtimeout_module>\n\tRequestReadTimeout header=20-40,MinRate=500 body=20,MinRate=500\n</IfModule>\n<IfModule mod_userdir.c>\n|" /etc/httpd/conf/httpd.conf
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 service httpd restart
+ip=$(/sbin/ifconfig eth1 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+echo "
+Thuc hien tan cong mang o may kali: perl slowloris.pl -dns $ip
+"
--- suricata_ubuntu16.sh
+++ suricata_ubuntu16.sh
@@ -18,8 +18,12 @@ echo "CREATE TEST.RULES"
 
 cat > /etc/suricata/rules/test.rules <<\EOF
 alert icmp any any -> $HOME_NET any (msg:"ICMP connection attempt"; sid:1000002; rev:1;) 
-drop icmp any any -> $HOME_NET any (msg:"ICMP connection attempt"; sid:1000002; rev:1;) 
 alert tcp any any -> $HOME_NET 23 (msg:"TELNET connection attempt"; sid:1000003; rev:1;)
+alert tcp any any -> $HOME_NET 22 (msg: "NMAP TCP Scan";sid:10000005; rev:2; )
+alert tcp any any -> $HOME_NET 22 (msg:"Nmap XMAS Tree Scan"; flags:FPU; sid:1000006; rev:1; )
+alert tcp any any -> $HOME_NET 22 (msg:"Nmap FIN Scan"; flags:F; sid:1000008; rev:1;)
+alert tcp any any -> $HOME_NET 22 (msg:"Nmap NULL Scan"; flags:0; sid:1000009; rev:1; )
+alert udp any any -> $HOME_NET any ( msg:"Nmap UDP Scan"; sid:1000010; rev:1; )
 EOF
 
 sed -i "s|default-rule-path: /var/lib/suricata/rules|default-rule-path: /etc/suricata/rules|" /etc/suricata/suricata.yaml
@@ -34,6 +38,13 @@ echo "chinh sua file /etc/suricata/suricata.yaml:
 - rule-files: - test.rules
 Chay chuong trinh: (rm -rf /var/run/suricata.pid) /usr/bin/suricata -D -c /etc/suricata/suricata.yaml -i  ens33
 Xem log: tail -50f /var/log/suricata/fast.log
+* test voi nmap:
+- nmap -sP $ip --disable-arp-ping
+- NMAP TCP Scan: nmap -sT -p22 $ip
+- NMAP XMAS Scan (lay flag: FIN, PSH, URG): nmap -sX -p22 $ip
+- NMAP FIN Scan (lay flag FIN): nmap -sF -p22 $ip
+- NMAP NULL Scan (TCP khong chua flag): nmap -sN -p22 $ip
+- NMAP UDP Scan: nmap -sU -p68 $ip
 "
 
 
